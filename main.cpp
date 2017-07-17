26787ba2197ea5aeaf36cb29028d91bacefc5fc2
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-17 19:55:28 +0200

    integrate basic code example

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int value {10};
+
+int& get() {
+    return value;
+}
+
+auto getValue() {
+    return get();
+}
+
+int main() {
+
+    auto destination = getValue();
+    destination += 5;
+    std::cout << value << std::endl;
+
+    return EXIT_SUCCESS;
+}
